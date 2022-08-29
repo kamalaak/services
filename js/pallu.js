2aa18adf73cfe5cfaded76f00255144092cd29c0
kamalaak <kamalstiries007@gmail.com>
2022-08-29 13:30:55 +0530

    slide show added

--- js/pallu.js
+++ js/pallu.js
@@ -7,3 +7,51 @@ function buttontoggel(){
         pallu.classList.add('hidden')
     }
 }
+
+
+
+// slide part 
+let slider = document.getElementsByClassName('slide-content');
+dots= document.getElementsByClassName('dots');
+sl = 1;
+
+function right(n){
+
+    show(sl +=n)
+
+};
+
+
+
+
+
+show(sl)
+
+
+function bu(n){
+    show(sl = n);
+}
+
+function show (n){
+    let i;
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].classList.add('hidden');
+        
+    }
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.add('bg-gray-800')
+        
+    }
+    if (n>slider.length) {
+        sl=1;
+    }
+    if (n<1) {
+        sl = slider.length;
+    }
+   slider[sl-1].classList.remove('hidden')
+   dots[sl-1].classList.remove('bg-gray-800')
+}
+
+
+
+// slide end here 
